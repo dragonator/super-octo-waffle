779ec1506c75d5c6bd696a86b8f2e051e45d4bd0
Teodor Draganov <draganov93@gmail.com>
2019-11-24 18:42:52 +0200

    Separate query call to a function

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,28 +11,33 @@ import (
 	"github.com/dragonator/super-octo-waffle/types"
 )
 
-func FetchPinnedItemsHandler(c *gin.Context) {
-	organization := c.Param("organization")
+func FetchPinnedItemsHandler(context *gin.Context) {
 	variables := map[string]interface{}{
-		"organization": githubv4.String(organization),
+		"organization": githubv4.String(context.Param("organization")),
 	}
 
-	client := createGithubClient(c)
+	client := createGithubClient(context)
 	query := types.OrganizationsPinnedItems{}
-
-	err := client.Query(c, &query, variables)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-	}
-
-	c.JSON(http.StatusOK, query)
+	sendQuery(client, context, &query, variables)
 }
 
-func createGithubClient(c *gin.Context) *githubv4.Client {
+	}
+
+}
+
+func createGithubClient(context *gin.Context) *githubv4.Client {
 	tokenSource := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
 	)
-	httpClient := oauth2.NewClient(c, tokenSource)
+	httpClient := oauth2.NewClient(context, tokenSource)
 
 	return githubv4.NewClient(httpClient)
+}
+
+func sendQuery(client *githubv4.Client, context *gin.Context, query interface{}, variables map[string]interface{}) {
+	err := client.Query(context, query, variables)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, err)
+	}
+	context.JSON(http.StatusOK, query)
 }
