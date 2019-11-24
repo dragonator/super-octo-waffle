df6cc51fba2eb4697548d43c1bbffbd0c9141a70
Teodor Draganov <draganov93@gmail.com>
2019-11-24 22:10:56 +0200

    Make REST API call to Github and populate custom strucct for response

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"os"
 
@@ -28,6 +30,15 @@ func FetchPinnedItemsHandler(context *gin.Context) {
 }
 
 func FetchRepositoryDataHandler(context *gin.Context) {
+	commits := make([]Commit)
+	for commit := range requestCommitsInJSON(context) {
+		append(commits, Commit{
+			Message:      commit["commit"]["message"],
+			Author:       commit["committer"]["name"],
+			AuthoredDate: commit["committer"]["date"],
+		})
+	}
+
 	variables := map[string]interface{}{
 		"organization": githubv4.String(context.Param("organization")),
 		"repository":   githubv4.String(context.Param("repository")),
@@ -38,6 +49,14 @@ func FetchRepositoryDataHandler(context *gin.Context) {
 	err := client.Query(context, query, variables)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, err)
+	}
+
+	repository := types.Repository{
+		Name:          query.Repository.Name,
+		NameWithOwner: query.Repository.NameWithOwner,
+		Readme:        query.Readme,
+		PackageJSON:   query.PackageJSON,
+		Commits:       commits,
 	}
 
 	context.JSON(http.StatusOK, repository)
@@ -52,3 +71,25 @@ func createGithubClient(context *gin.Context) *githubv4.Client {
 	return githubv4.NewClient(httpClient)
 }
 
+func requestCommitsInJSON(context *gin.Context) *http.Response {
+	url = fmt.Sprintf("https://api.github.com/repos/%s/%s/commits",
+		context.Param("organization"),
+		context.Param("repository"))
+
+	resp, response_err := http.Get(url)
+	if response_err != nil {
+		context.JSON(http.StatusInternalServerError, response_err)
+	}
+
+	body, read_err := ioutil.ReadAll(resp.Body)
+	if read_err != nil {
+		context.JSON(http.StatusInternalServerError, read_err)
+	}
+
+	commits_json, marshal_err := json.Marshal(body)
+	if marshal_err != nil {
+		context.JSON(http.StatusInternalServerError, marshal_err)
+	}
+
+	return &resp
+}
