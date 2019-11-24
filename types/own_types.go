3311171a77f1387bad3140a8ccf8a9abc6480a9b
Teodor Draganov <draganov93@gmail.com>
2019-11-24 22:04:04 +0200

    Add own struct types

---
+++ types/own_types.go
@@ -0,0 +1,29 @@
+package types
+
+type PinnedRepositories struct {
+	Organization string
+	TotalCount   int32
+	Repositories []struct {
+		Name              string
+		NameWithOwner     string
+		LicenseName       string
+		ContributorsCount int32
+		ReleasesCount     int32
+		DefaultBranchName string
+		CommitsCount      int32
+	}
+}
+
+type Commit struct {
+	Message      string
+	Author       string
+	AuthoredDate string
+}
+
+type Repository struct {
+	Name          string
+	NameWithOwner string
+	Readme        string
+	PackageJSON   string
+	Commits       []Commit
+}
