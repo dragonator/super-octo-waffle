4223dca286146571e029ed7e3917323e0858a716
Teodor Draganov <draganov93@gmail.com>
2019-11-26 23:13:50 +0200

    Generate pinned-repositories component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { PinnedRepositoriesComponent } from './pinned-repositories/pinned-repositories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PinnedRepositoriesComponent
   ],
   imports: [
     BrowserModule,
---
+++ ui/src/app/pinned-repositories/pinned-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pinned-repositories',
+  templateUrl: './pinned-repositories.component.html',
+  styleUrls: ['./pinned-repositories.component.scss']
+})
+export class PinnedRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
