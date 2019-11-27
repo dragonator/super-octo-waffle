ee35ceec391ecb56db5484382753200f223b4ff6
Teodor Draganov <draganov93@gmail.com>
2019-11-27 02:02:43 +0200

    Define routes

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent }        from './home/home.component';
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule }             from '@angular/core';
+import { AuthGuardService }     from './auth-guard.service';
+import { CallbackComponent }    from './callback/callback.component';
+import { PinnedReposComponent } from './pinned-repos/pinned-repos.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'pinnedRepos', component: PinnedReposComponent,  canActivate: [AuthGuardService] },
+  { path: 'callback', component: CallbackComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
