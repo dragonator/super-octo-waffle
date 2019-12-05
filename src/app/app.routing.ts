372c7e711ac31e407b1984083a571471c08562a4
Teodor Draganov <draganov93@gmail.com>
2019-12-05 21:10:51 +0200

    Redirect al unsupported routes to home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,18 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule }             from '@angular/core';
+
 import { AuthGuard }            from '@app/_helpers';
 import { HomeComponent }        from '@app/home';
 import { CallbackComponent }    from '@app/callback';
 import { PinnedReposComponent } from '@app/pinned-repos';
 
 const routes: Routes = [
-  { path: '',      redirectTo: 'home',                         pathMatch: 'full' },
-  { path: 'login', redirectTo: 'dev-a638a1un.auth0.com/login', pathMatch: 'full'},
-
   { path: 'login/callback',            component: CallbackComponent },
   { path: 'home',                      component: HomeComponent },
   { path: 'pinnedRepos/:organization', component: PinnedReposComponent,  canActivate: [AuthGuard] },
+
+  { path: 'login', redirectTo: 'dev-a638a1un.auth0.com/login', pathMatch: 'full'},
+  { path: '**',    redirectTo: 'home',                         pathMatch: 'full' },
 ];
 
 @NgModule({
