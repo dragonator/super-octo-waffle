d564aaef80c6450b241f03036bce9d44d4cb539a
Teodor Draganov <draganov93@gmail.com>
2019-12-05 21:12:14 +0200

    Show pinned repos on successful API call

--- src/app/pinned-repos/pinned-repos.component.ts
+++ src/app/pinned-repos/pinned-repos.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute }               from '@angular/router';
+
+import { PinnedReposService } from '@app/_services/pinned-repos.service';
+import { PinnedRepos        } from '@app/_models';
 
 @Component({
-  selector: 'app-pinned-repos',
+  selector:    'app-pinned-repos',
   templateUrl: './pinned-repos.component.html',
-  styleUrls: ['./pinned-repos.component.scss']
+  styleUrls:  ['./pinned-repos.component.scss']
 })
-export class PinnedReposComponent implements OnInit {
+export class PinnedReposComponent implements OnInit, OnDestroy {
+  public pinnedRepos: PinnedRepos;
+  private sub: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private pinnedReposService: PinnedReposService) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.pinnedReposService.getPinnedReposList(params['organization'])
+        .subscribe(data => {
+          this.pinnedRepos = data;
+        })
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
